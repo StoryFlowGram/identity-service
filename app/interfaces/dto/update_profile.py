6275e0c7bb1c60f514d9d9f59465b0342845f22d
Squilix01 <Squilix01@no-reply.com>
2025-08-26 13:45:07 +0300

    refactor: remove or move old files after layer refactor

--- app/interfaces/dto/update_profile.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional
-
-class UpdateProfileDTO(BaseModel):
-    first_name: Optional[str] = Field(None, description="Имя пользователя")
-    last_name: Optional[str] = Field(None, description="Фамилия пользователя")
-    username: Optional[str] = Field(None, description="Имя пользователя в системе")
-    avatar_url: Optional[str] = Field(None, description="URL аватара пользователя")
