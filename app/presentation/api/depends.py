53e89323142c3790353f97dcbd51491de651ca20
Squilix <Squilix@example.com>
2026-02-08 20:50:14 +0200

    refactor: remove prefix from profile_router and moved this prefix into gateway. rewrote logic get_current_user in depends

--- app/presentation/api/depends.py
+++ app/presentation/api/depends.py
@@ -1,10 +1,5 @@
-from fastapi import Depends, HTTPException
+from fastapi import HTTPException, Header
 from fastapi.security import HTTPBearer
-from jwt import InvalidTokenError, ExpiredSignatureError
-from loguru import logger
-
-
-from app.application.interfaces.token_service import AbstractJWTTokenService
 
 bearer_scheme = HTTPBearer()
 
@@ -18,30 +13,9 @@ def get_oauth_service():
     raise NotImplementedError("Должна быть реализация в Infra слое")
 
 
-def get_current_user(
-    token: str = Depends(bearer_scheme), 
-    token_service: AbstractJWTTokenService = Depends(get_jwt_token_service)
-):
-    jwt_token = token.credentials
-    try: 
-        user_id = token_service.get_user_id(jwt_token)
-        return user_id
-    except ExpiredSignatureError:
-        raise HTTPException(
-            status_code=401,
-            detail="Токен истёк",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    except InvalidTokenError:
-        raise HTTPException(
-            status_code=401,
-            detail="Токен не валидный",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    except Exception as e:
-        logger.error(f"Ошибка при проверке токена: {e}")
-        raise HTTPException(
-            status_code=401,
-            detail="Невалидный токен",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
+async def get_current_user(x_user_id: str = Header(None, alias="X-User-Id")) -> int:
+    if x_user_id is None:
+        raise HTTPException(status_code=401, detail="X-User-Id заголовок отсутствует")
+    if not x_user_id.isdigit():
+        raise HTTPException(status_code=400, detail="X-User-Id не является числом")
+    return int(x_user_id)
